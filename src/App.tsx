d8c76685ecafddb7dbfaedd17d7145e4a7252f98
HisMonDon <revenge.ez.123@gmail.com>
2026-03-15 13:54:27 -0400

    changed class flex (removed gap from all the classes made

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 //import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,101 +6,24 @@ function App() {
 
   return (
     <>
-      <section>
-        {/* <div className="hero">
-        </div> */}
-        <div className="columnstart">
-          <div className="rowstart" style={{ padding: '20px 16px' }}>
-            <span style={{ fontSize: '32px', fontWeight: 'bold', color: '#b568f0' }}>
-              Kosmos
-            </span>
-          </div>
+      <div className="columncenter" style={{ padding: '30px' }}>
+        <div className="rowstart">
+          <span style={{ fontSize: '32px', fontWeight: 'bold', color: '#b568f0' }}>
+            Kosmos
+          </span>
+          <div className="ticks"></div>
         </div>
-      </section>
 
+        <div className="rowcenter">
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+          <section>
+            {/* <div className="hero">
+            </div> */}
+            <div className="columnstart">
+            </div>
+          </section>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
+      </div>
       <section id="spacer"></section>
     </>
   )
